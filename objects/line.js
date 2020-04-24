60a3b3854e3620502065ac8e813154a1692a9bdd
Haouari haithem <57036855+wolfpat01@users.noreply.github.com>
2020-04-24 03:03:55 +0100

    updating style

--- objects/line.js
+++ objects/line.js
@@ -9,9 +9,9 @@ module.exports = class ChatLine {
         return `
         <div class="row rounded bg-light  pr-1 col-12 mt-1" style="Border-radius:550px;">
         <div class="col-3 bg-info">
-        <img src="${this.img}"class="rounded"/>
+        <img src="${this.img}"class="rounded pt-1"/>
         </div>
-        <div class="col-9 bl-9 border-left-1 bg-secondary border-light">                
+        <div class="col-9 bg-secondary pt-1 pb-1">                
         <h4 class="col-12 w-100" style="color:${this.nameColor};">${this.name}</h2>
         <a class="col-12 w-100 h-100 "style="color:white;">${this.text}</a>
         </div>
